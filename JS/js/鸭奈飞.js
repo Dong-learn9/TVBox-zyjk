589f262affc9bd57da5b94ee9d9c6a190206322e
莫名的悲伤 <598500782@qq.com>
2022-12-03 23:57:08 +0800

    add

---
+++ JS/js/鸭奈飞.js
@@ -0,0 +1,9 @@
+var rule = Object.assign(muban.mxpro,{
+title:'鸭奈飞',
+//host:'https://yanetflix.com',
+host:'https://yanetflix.tv/',
+url:'/vodshow/fyclass--------fypage---.html',
+//class_parse:'.navbar-items&&li;a&&Text;a&&href;.*/(.*?).html',
+class_name:'电视剧&电影&综艺&动漫',
+class_url:'lianxuju&dianying&zongyi&dongman',
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# TVBOXZY
+
+TVBOX
