d1f8f2cb5f62838fcb39c026199b78a16a8ebe58
莫名的悲伤 <598500782@qq.com>
2022-12-17 17:13:30 +0800

    更新

--- JS/js/酷客影院.js
+++ JS/js/酷客影院.js
@@ -3,8 +3,8 @@ var rule = {
     host:'http://www.2kuke.com',
     // homeUrl:'/',
     url:'/list/fyclass_fypage.html',
-    //searchUrl:'/vodsearch/**----------fypage---.html',
-    searchable:0,//是否启用全局搜索,
+    searchUrl:'/search.php#searchword=**;post',
+    searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
     headers:{//网站的请求头,完整支持所有的,常带ua和cookies
@@ -20,6 +20,7 @@ var rule = {
     推荐:'.stui-vodlist.clearfix;.stui-vodlist__box;a&&title;a&&data-original;.pic-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.stui-vodlist.clearfix li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    二级:{"title":"h1.line1&&Text;.stui-content__detail p:eq(0)&&Text","img":".lazyload&&data-original","desc":";;;.stui-content__detail p:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text","content":".stui-content__detail p:eq(4)&&Text","tabs":".nav.nav-tabs.pull-right&&li","lists":".stui-content__playlist:eq(#id) li"},
+    二级:{"title":"h1.line1&&Text;.stui-content__detail p:eq(0)&&Text","img":".lazyload&&data-original","desc":";;;.stui-content__detail p:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text","content":".stui-content__detail p:eq(-1)&&Text","tabs":".nav.nav-tabs.pull-right&&li","lists":".stui-content__playlist:eq(#id) li"},
+    搜索:'ul.stui-vodlist__media&&li;*;*;*;*'
     
 }
