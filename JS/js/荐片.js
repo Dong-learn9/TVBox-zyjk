81c789e27a8037c26fd7299101a5f150fa21ae4b
莫名的悲伤 <598500782@qq.com>
2024-02-04 02:29:52 +0800

    1

--- JS/js/荐片.js
+++ JS/js/荐片.js
@@ -5,8 +5,8 @@ var rule = {
     homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
     // url:'/api/crumb/list?area=0&category_id=fyclass&page=fypage&type=0&limit=24&fyfilter',
     url:'/api/crumb/list?area=0&page=fypage&type=0&limit=24&fyfilter',
-    class_name:'全部&电视剧&电影&综艺&动漫',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
-    class_url:'0&2&1&4&3',
+    class_name:'全部&电影&电视剧&动漫&综艺',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
+    class_url:'0&1&2&3&4',
     detailUrl:'/api/node/detail?channel=wandoujia&token=&id=fyid',//二级详情拼接链接(json格式用)
     searchUrl:'/api/video/search?key=**&page=fypage',
     searchable:2,
@@ -44,7 +44,7 @@ var rule = {
     }],
     lazy:'',
     // double:true,
-    图片来源:'@Referer=www.jianpianapp.com@User-Agent=jianpian-version353',    
+    图片来源:'@Referer=www.jianpianapp.com@User-Agent=jianpian-version353',
     // 推荐:'json:data;room;*;cover;*;*',
     推荐:'json:.video;*;*;*;*',
     一级:'json:data;title;path;playlist.title;id',
